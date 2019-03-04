8eb74b2069e7c748115cad7adbdb85f45cd72fd5
beccaelenzil <beccaelenzil@gmail.com>
2019-03-04 14:18:12 -0800

    phone number exercise

--- textbook-curriculum/00-programming-fundamentals/regex/regex_csv.rb
+++ textbook-curriculum/00-programming-fundamentals/regex/regex_csv.rb
@@ -1,6 +1,23 @@
+=begin
+
 # phone_numbers.txt
 (206) 555-1234
-(425) 555-9999
-(406) 555-1818
-(206) 555-8888
-(425) 555-8872
+425-555-9999
+406.555.1818
++1 206 555 8888
+4255558872
+
+=end
+
+pattern = /^.*(\d{3}).*(\d{3}).*(\d{4})$/
+phones_numbers = ['(206) 555-1234','425-555-9999','406.555.1818','+1 206 555 8888','4255558872']
+new_phone_numbers = []
+
+phones_numbers.each do |number|
+    results = pattern.match(number)
+    new_number = "(#{results[1]}) #{results[2]}-#{results[3]}"
+    new_phone_numbers.push(new_number)
+end
+
+puts new_phone_numbers
+
