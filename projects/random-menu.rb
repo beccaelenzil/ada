995748192fbd1db116f463595a8b9f537f649628
beccaelenzil <beccaelenzil@gmail.com>
2019-07-22 12:00:18 -0700

    completed day 1 hw: random-menu.rb

---
+++ projects/random-menu.rb
@@ -0,0 +1,50 @@
+require 'awesome_print'
+
+# Hard coded
+
+adjectives = ["hot", "cold", "warm", "spicy"]
+styles = ["stir fried", "steamed", "baked", "toasted"]
+foods = ["noodles", "candy", "tofu", "cereal"]
+
+n = foods.length
+
+# # Ask for user input
+
+# adjectives = []
+# styles = []
+# foods = []
+
+# puts "Let's build our word lists for our random menu."
+# puts "How many menu items would you like?"
+# n = gets.chomp
+# n = n.to_i
+
+# (1..n).each do |i|
+#     puts "Collecting word set number #{i}:"
+#     print "Give me an adjective: "
+#     adjectives << gets.chomp
+#     print "Give me a cooking style: "
+#     styles << gets.chomp
+#     print "Give me a food: "
+#     foods << gets.chomp 
+# end
+
+menu = []
+
+(1..n).each do
+    # random generation
+    adjective = adjectives.sample
+    style = styles.sample
+    food = foods.sample
+    menu_item = adjective + " " + style + " " + food
+
+    # puts item in menu
+    menu << menu_item
+
+    # remove used items
+    adjectives.delete(adjective)
+    styles.delete(style)
+    food.delete(food)
+end
+
+ap menu
