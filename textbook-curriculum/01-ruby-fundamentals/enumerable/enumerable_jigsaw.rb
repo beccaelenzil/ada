6ca4c4268be4f762c9953218803b89feab532844
beccaelenzil <beccaelenzil@gmail.com>
2019-08-08 09:43:56 -0700

    organization

--- textbook-curriculum/01-ruby-fundamentals/enumerable/enumerable_jigsaw.rb
+++ textbook-curriculum/01-ruby-fundamentals/enumerable/enumerable_jigsaw.rb
@@ -1,12 +1,25 @@
 # enumerable methods
 
+#     What does this method do on a high level?
+#     What does it return?
+#     What does the code block do?
+#     What kind of syntax does it require?
+#     What does a code example look like?
 
-#******reduce******
-
-#(5..10).reduce(:+)   sums numebers 5-- 10
+#reduce
+#-----------------
 
 #all? & any?
+#-----------------
+
 #select & reject
+#-----------------
+
 #uniq & sum
+#-----------------
+
 #min, max, min_by, & max_by
+#-----------------
+
 #sort_by
+#-----------------
