12ce9945ed67ad7780fe1a18c716fed87f89e9a1
beccaelenzil <beccaelenzil@gmail.com>
2019-11-10 07:00:15 -0800

    symbol and word operations

--- by_week/Week15/js-calculator/calculator.js
+++ by_week/Week15/js-calculator/calculator.js
@@ -18,39 +18,52 @@ const calculator = function(error, result) {
   const operation = result.operation
   const num1 = parseFloat(result.num1)
   const num2 = parseFloat(result.num2)
-  let solution;
+  let solution = 'hi';
   
   switch(operation){
     case '+' || 'add':
       solution = num1 + num2;
       break;
     case 'add':
-        solution = num1 + num2;
-        break;
-    case '-' || 'subtract':
+      solution = num1 + num2;
+      break;
+    case '-':
       solution = num1 - num2;
       break;
-    case '*' || 'multiply':
+    case 'subtract':
+      solution = num1 - num2;
+      break;
+    case '*':
       solution = num1 * num2;
       break;
-    case '/' || 'divide':
+    case 'multiply':
+      solution = num1 * num2;
+      break;
+    case '/':
+      solution = num1 / num2;
+      break;
+    case 'divide':
       solution = num1 / num2;
       break;
     default:
       console.log("We don't support that operation")
-  }  
-  printEq(num1, num2, operation, solution)
+  }
+  if (solution === 'hi'){
+    console.log('sorry charley')
+  }else{
+    printEq(num1, num2, operation, solution)
+  }
   return solution
 }  
 //https://www.npmjs.com/package/prompt
 let schema = {
   properties: {
     num1: {
-      pattern: /[-.]?\d+\.?\d+/,
+      pattern: /[-.]?\d+\.?\d*/,
       message: 'Must enter a number'
     },
     num2: {
-      pattern: /[-.]?\d*\.?\d*/,
+      pattern: /[-.]?\d+\.?\d*/,
       message: 'Must enter a number'
     },
     operation: {    }
@@ -64,6 +77,6 @@ prompt.start();
 //then call the function `calculator` with the results
 console.log('Enter two numbers and one of the following operations: add (+), subtract (-), multiply (*), divide (/)')
 
-
 prompt.get(schema, calculator);
+
 
